cc054a4a6559060f7645d844589a833d46a82d8d
kviramga <kviramga@purdue.edu>
2025-08-08 09:16:15 -0500

    links page with animations, images, and updated heading

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import AboutUs from './AboutUs';
 import ExecutiveBoard from './ExecutiveBoard';
+import Links from './Links';
 
 function App() {
   return (
     <div className="App">
       <AboutUs />
+      <Links />
       <ExecutiveBoard />
     </div>
   );
