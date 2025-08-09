07a365202faac8abf77aa92ae52fd2f0ec1478fe
Mikyla Farnell <myffarnell@gmail.com>
2025-08-09 04:14:09 -0400

    updated FAQs

--- src/FaqCard.js
+++ src/FaqCard.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
 import './FaqCard.css'
 
 export default function FaqCard({ front, back, backColor }) {
@@ -12,10 +14,12 @@ export default function FaqCard({ front, back, backColor }) {
     >
       <div className="faq-card-container">
         <div className="faq-card-front">
+          <FontAwesomeIcon className="faq-card-flip-icon" icon={faSyncAlt}/>
           <p>{front}</p>
         </div>
-        
+
         <div className="faq-card-back" style={{ backgroundColor: backColor }}>
+          <FontAwesomeIcon className="faq-card-flip-icon" icon={faSyncAlt}/>
           <p>{back}</p>
         </div>
       </div>
