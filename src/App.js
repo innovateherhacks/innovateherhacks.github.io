148ad2c1bfad19338e52ec037ac780bcf0f16776
manasaattuluri <mattulur@purdue.edu>
2025-08-12 04:24:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import './App.css';
 import AboutUs from './AboutUs';
 import ExecutiveBoard from './ExecutiveBoard';
@@ -12,4 +12,40 @@ function App() {
   );
 }
 
+export default App;*/
+
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+import AboutUs from './AboutUs';
+import ExecutiveBoard from './ExecutiveBoard';
+import Teams from './Teams';
+import TeamLanding from './TeamLanding';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Redirect root to AboutUs */}
+          <Route path="/" element={<Navigate to="/about" />} />
+
+          {/* Main pages */}
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/executive-board" element={<ExecutiveBoard />} />
+          <Route path="/teams" element={<Teams />} />
+
+          {/* Dynamic team pages */}
+          <Route path="/teams/:teamId" element={<TeamLanding />} />
+
+          {/* Optional: Catch-all for undefined routes */}
+          <Route path="*" element={<div>404 - Page Not Found</div>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
 export default App;
+
