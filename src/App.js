32d1a98d8919a8d12e92e145852e9c67de3b95f0
SaanviSharma22 <108912946+SaanviSharma22@users.noreply.github.com>
2025-09-25 13:33:03 -0400

    Completed Tracks page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './App.css'
 import AboutUs from './AboutUs'
-import Tracks from './Tracks';
+/*import Tracks from './Tracks'; */
+import TracksPage from './TracksPage';
 import ExecutiveBoard from './ExecutiveBoard'
 import Links from './Links'
 import FAQs from './FAQs'
@@ -11,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <AboutUs/>
-      <Tracks />
+      <TracksPage />
       <Links/>
       <ExecutiveBoard/>
       <FAQs/>
