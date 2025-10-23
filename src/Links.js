f303d0ecd86d20c0908583e3db0a36a6c8a7409c
kviramga <kviramga@purdue.edu>
2025-10-23 19:32:35 -0400

    added alert pop up for users to come back november 25 to sign up

--- src/Links.js
+++ src/Links.js
@@ -2,22 +2,36 @@ import React from 'react';
 import './Links.css';
 
 function Links() {
+  const handleClick = (role) => {
+    let message = '';
+
+    if (role === 'Participant') {
+      message = 'Sign-ups for Participants open on November 25th! Please check back then.';
+    } else if (role === 'Mentor') {
+      message = 'Sign-ups for Mentors open on December 1st! Please check back then.';
+    } else if (role === 'Volunteer') {
+      message = 'Sign-ups for Volunteers open on December 1st! Please check back then.';
+    }
+
+    alert(message);
+  };
+
   return (
     <div className="links-container">
       <h2 className="links-heading">READY? CHOOSE YOUR MODE:</h2>
       <div className="links-boxes">
-        <a className="link-item" href="https://example.com/participant" target="_blank" rel="noopener noreferrer">
+        <div className="link-item" onClick={() => handleClick('Participant')}>
           <img className="link-img" src="/Participants.png" alt="Participant" />
           <div className="box-label">Participant</div>
-        </a>
-        <a className="link-item" href="https://example.com/mentor" target="_blank" rel="noopener noreferrer">
+        </div>
+        <div className="link-item" onClick={() => handleClick('Mentor')}>
           <img className="link-img" src="/Mentors.png" alt="Mentor" />
           <div className="box-label">Mentor</div>
-        </a>
-        <a className="link-item" href="https://forms.gle/J5oG23hM2XbGd3X87" target="_blank" rel="noopener noreferrer">
+        </div>
+        <div className="link-item" onClick={() => handleClick('Volunteer')}>
           <img className="link-img" src="/Volunteers.png" alt="Volunteer" />
           <div className="box-label">Volunteer</div>
-        </a>
+        </div>
       </div>
     </div>
   );
