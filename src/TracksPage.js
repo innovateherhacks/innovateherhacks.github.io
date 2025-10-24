27955d069ff2e2c48b02412d1f85641c11e06597
SaanviSharma22 <108912946+SaanviSharma22@users.noreply.github.com>
2025-10-24 18:20:28 -0400

    Added new tracks

--- src/TracksPage.js
+++ src/TracksPage.js
@@ -3,11 +3,11 @@
 import React from 'react';
 import './TracksPage.css';
 
-import img1 from './images/Tracks/purdue.png';
-import img2 from './images/Tracks/healthfitness.png';
-import img3 from './images/Tracks/geoconnections.png';
-import img4 from './images/Tracks/sustainability.png';
-import img5 from './images/Tracks/financial_lit.png';
+import img1 from './images/Tracks/Purdue (1).png';
+import img2 from './images/Tracks/Mind Matters.png';
+import img3 from './images/Tracks/[website] Finance Forward.png';
+import img4 from './images/Tracks/Culture Connect.png';
+import img5 from './images/Tracks/Art + Tech Fusion.png';
 
 const tracks = [
   { name: 'Purdue', image: img1, className: 'track-card gold' },
@@ -26,7 +26,7 @@ export default function TracksPage() {
           <div className={`track-item ${track.colorClass}`} key={track.name}>
             {/* Use imported image as src */}
             <img src={track.image} alt={track.name} className="track-icon" />
-            <div className="track-label">{track.name}</div>
+            {/* <div className="track-label">{track.name}</div> */}
           </div>
         ))}
       </div>
